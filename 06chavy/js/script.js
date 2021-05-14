8a6233fe52deb6b190b4d41e2e4e17c515995b93
eh0637 <kimeunh429@gmail.com>
2021-05-14 18:24:11 +0900

    up

--- 06chavy/js/script.js
+++ 06chavy/js/script.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
     auto:true,
     controls:false,
     pager:false
-  })
+  });
+
+  $(".cont_menu li").click(function(){
+    let t = $(this).index();
+    console.log(t)
+
+    $(".cont_menu li").removeClass("active");
+    $(this).addClass("active")
+    $(".tab_menuWrap .tab_contList").hide().eq(t).fadeIn();
+  });
+
+ $( ".accordion" ).accordion();
 });
