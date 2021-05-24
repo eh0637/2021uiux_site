3bb58e1b561a775aae9b750c637b9fb006e12e03
eh0637 <kimeunh429@gmail.com>
2021-05-24 09:55:23 +0900

    up

---
+++ 06chavy/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var mySlider = $(".slider").bxSlider({
+    auto:true,
+    controls:false,
+    pager:false
+  });
+
+
+  $(".accordion").on('click',function(){
+    $(this).next(".list_subCon").slideToggle(100);
+  });
+
+  $(".tab_menuWrap .tab_contList").hide().eq(1).show();
+  $(".cont_menu li").click(function(){
+    let t = $(this).index();
+    console.log(t)
+
+    $(".cont_menu li").removeClass("active");
+    $(this).addClass("active")
+    $(".tab_menuWrap .tab_contList").hide().eq(t).fadeIn();
+  });
+
+ // $( ".accordion" ).accordion();
+});
